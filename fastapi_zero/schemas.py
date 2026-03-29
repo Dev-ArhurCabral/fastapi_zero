cb61e31b24b88a955d6980a1950b90b4e651d6e2
Arthur Cabral <arthurcabral@hotmail.com.br>
2026-03-29 08:32:25 -0300

    Implementando rotas CRUD

--- fastapi_zero/schemas.py
+++ fastapi_zero/schemas.py
@@ -1,6 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 # Documantando schema de troca de mensagem
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,8 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
-    """
-    Esse teste tem 3 etapas (AAA)
-    A - Arrange - arranjo
-    A - Act     - Execura a coisa (o SUT)
-    A - Assert  - Garante que A é A
-    """
+def test_root_deve_retornar_ola_mundo(client):
     # Arrange
-    client = TestClient(app)
     # Act
     response = client.get('/')
     # Assert
@@ -21,11 +10,53 @@ def test_root_deve_retornar_ola_mundo():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_exercicio_aula_02():
+def test_creat_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }
 
-    client = TestClient(app)
 
-    response = client.get('/exercicio-html')
+def test_read_users(client):
+    resource = client.get('/users/')
+    assert resource.status_code == HTTPStatus.OK
+    assert resource.json() == {
+        'users': [{'id': 1, 'email': 'alice@example.com', 'username': 'alice'}]
+    }
 
-    assert '<h1> Olá Mundo </h1>' in response.text
-    assert response.status_code == HTTPStatus.OK
+
+def test_updateuser(client):
+    resource = client.put(
+        '/users/1',
+        json={
+            'username': 'Bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+    assert resource.status_code == HTTPStatus.OK
+    assert resource.json() == {
+        'username': 'Bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_user_delete(client):
+    resource = client.delete('/users/1')
+    assert resource.status_code == HTTPStatus.OK
+    assert resource.json() == {
+        'username': 'Bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
